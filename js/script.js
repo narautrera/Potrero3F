943a2fa0704720f54b18f31538945c9758276d54
narautrera <89810949+narautrera@users.noreply.github.com>
2021-10-31 21:07:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function modo()
+{document.body.style.backgroundColor = "#212F3C";
+document.body.style.backgroundImage = "none";
+document.body.style.color = "gray";
+document.getElementById("fondito1").style.backgroundColor = "black";
+document.getElementById("fondito1").style.color = "white";
+document.getElementById("fondito2").style.backgroundColor = "gray";
+document.getElementById("fondito2").style.color = "white";
+document.getElementById("fondito3").style.backgroundColor = "black";
+document.getElementById("fondito3").style.color = "white";
+document.getElementById("fondito4").style.backgroundColor = "black";
+document.getElementById("fondito4").style.color = "white";
+document.getElementById("fondito5").style.backgroundColor = "black";
+document.getElementById("fondito5").style.color = "white";
+}
+
+function datos()
+{alert ("Creado por: Community Development de Nara y Co. - 2021")}
